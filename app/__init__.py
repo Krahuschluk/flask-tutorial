cb314cc1e66b1eee4980ad328bb08edffc33fac3
Krah <fehuschluk@gmail.com>
2018-12-15 15:28:52 +0900

    Added chapter 11 with CSS

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
