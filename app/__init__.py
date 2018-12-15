d8fd504ac19f0395b2c7cb7ea18ff3ab99f92225
Krah <fehuschluk@gmail.com>
2018-12-15 20:14:12 +0900

    Added chapter 12 on dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
